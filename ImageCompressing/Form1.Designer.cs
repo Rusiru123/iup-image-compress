e8152453b53755d7552822b5a20a029f4909da96
rusiru1995 <rusiru@smallaxe.tech>
2020-04-25 13:48:52 +0530

    added the files

---
+++ ImageCompressing/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ImageCompressing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(641, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save and compress";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(373, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnBrowse_click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 146);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(660, 297);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 480);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ ImageCompressing/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing.Imaging;
+
+namespace ImageCompressing
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CompressImage(Image srcImage, int imageQuality, string savePath)
+        {
+            try
+            {
+                ImageCodecInfo jpegCodec = null;
+
+                EncoderParameter imageQualityParameter = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality,imageQuality);
+
+                ImageCodecInfo[] everyCodecs = ImageCodecInfo.GetImageEncoders();
+
+                EncoderParameters codecParameter = new EncoderParameters(1);
+                codecParameter.Param[0] = imageQualityParameter;
+
+                for (int x = 0; x < everyCodecs.Length; x++)
+                {
+                    if (everyCodecs[x].MimeType == "image/jpeg")
+                    {
+                        jpegCodec = everyCodecs[x];
+                        break;
+                    }     
+
+                }
+
+            }
+
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+
+        private void btnBrowse_click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Title = "Select an image ";
+            od.Filter = "Jpeg Images(*.jpg)|*.jpg";
+            od.Filter += "|Png Images(*.png)|*.png";
+            od.Filter += "|Bitmap Images(*.bmp)|*.bmp";
+            od.Filter += " |All(*.JPG,*.PNG,*.BMP)|*.JPG;*.PNG;*.BMP";
+            od.FilterIndex = 1;
+
+            if(od.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                 textBox1.Text = od.FileName;
+                Image imageIn  = Image.FromFile(textBox1.Text);
+                pictureBox1.Image = imageIn;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "")
+            {
+                MessageBox.Show("Please load Image first");
+            }
+            else if(textBox1.Text.Contains(".jpg"))
+            {
+                CompressImage(Image.FromFile(textBox1.Text), 30, textBox1.Text.Insert(textBox1.Text.Length - 4, "JPEG Image compressed"));
+                MessageBox.Show("Image compressed");
+            }
+            else
+            {
+                string pr = textBox1.Text.Insert(textBox1.Text.Length - 4, "JPEG Image compressed");
+                string p = "abcdef"; 
+
+                CompressImage(Image.FromFile(textBox1.Text), 30, textBox1.Text.Insert(textBox1.Text.Length - 4, "JPEG Image compressed").Substring(0, pr.Length - 4) + ".jpg");
+                MessageBox.Show("Image compressed");
+            }
+        }
+
+    }
+}
